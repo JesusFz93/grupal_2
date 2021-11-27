33df64f657d548f7a3d7e2f4042844a441ee4a91
Alberto <90433622+aserruto@users.noreply.github.com>
2021-11-27 15:19:38 -0500

    Add files via upload
    Llenado del select con los datos de los users.

---
+++ request.js
@@ -0,0 +1,22 @@
+    // Funcion que llenar las opciones en el select 
+    
+    function obtenerValorDelApi() {
+
+        const url = 'https://jsonplaceholder.typicode.com/users';
+                
+        fetch(`${url}`)
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => {
+
+                const sel = document.getElementById("select"); // capturamos el elemento select del dom
+                                                
+                for(var i = 0; i < resultado.length; i++) {
+                    var opt = document.createElement('option');
+                    opt.innerHTML = resultado[i].name;
+                    console.log(opt);
+                    sel.appendChild(opt);
+                }
+        });
+    }
+
+obtenerValorDelApi();
